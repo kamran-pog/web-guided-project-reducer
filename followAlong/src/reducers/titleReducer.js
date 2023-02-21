2a7318f5427eec688b7400c48246fdbe14698d19
kamran-pog <kamran.k0807@gmail.com>
2023-02-20 16:39:24 -0800

    done

---
+++ followAlong/src/reducers/titleReducer.js
@@ -0,0 +1,17 @@
+import { SET_EDITING, SET_TITLE, SET_NEWTITLETEXT } from "../actions/titleActions";
+
+console.log("const set_editing:", SET_EDITING)
+const reducer = (state, action) => {
+    switch(action.type) {
+        case(SET_TITLE):
+            return({...state, title: action.payload});
+        case(SET_EDITING):
+            return({...state, editing: action.payload});
+        case(SET_NEWTITLETEXT):
+            return({...state, newTitleText: action.payload});
+        default:
+            return state;
+    }
+}
+
+export default reducer;
